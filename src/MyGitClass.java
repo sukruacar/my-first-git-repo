96cd9c55098b35d0632e39f5a533707fbedfe574
Sukru Acar <acar3410@gmail.com>
2022-07-16 22:47:01 +0300

    third commit

--- src/MyGitClass.java
+++ src/MyGitClass.java
@@ -4,6 +4,8 @@ public class MyGitClass {
 
         System.out.println("I am learning GitHub");
         System.out.println("abc");
+        System.out.println("I am adding new code...");
+        System.out.println("I am adding one more");
 
 
     }
